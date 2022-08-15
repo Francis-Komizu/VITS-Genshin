4ca3a2d1619d214ed4bce3f26635f0b821dc0d80
FrancisHu <87693204+Francis-Komizu@users.noreply.github.com>
2022-08-15 22:07:09 +0800

    Update preprocess.py

--- preprocess.py
+++ preprocess.py
@@ -5,9 +5,9 @@ from utils import load_filepaths_and_text
 if __name__ == '__main__':
   parser = argparse.ArgumentParser()
   parser.add_argument("--out_extension", default="cleaned")
-  parser.add_argument("--text_index", default=1, type=int)
-  parser.add_argument("--filelists", nargs="+", default=["filelists/", "filelists/ljs_audio_text_test_filelist.txt"])
-  parser.add_argument("--text_cleaners", nargs="+", default=["english_cleaners2"])
+  parser.add_argument("--text_index", default=2, type=int)
+  parser.add_argument("--filelists", nargs="+", default=["filelists/train_filelist", "filelists/val_filelist.txt"])
+  parser.add_argument("--text_cleaners", nargs="+", default=["chinese_cleaners1"])
 
   args = parser.parse_args()
     
